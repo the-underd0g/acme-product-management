e7468271e4ee18f478257ab2f8643ffbd39f7f02
UnderD0g <the.underd0g@outlook.com>
2019-01-06 16:21:33 -0300

    adding route and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { template } from "@angular/core/src/render3";
 @Component({
   selector: 'pm-root',
   template: `
-    <div>
-      <h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
-    </div>
-    `
+  <nav class='navbar navbar-expand navbar-light bg-light'>
+    <a class='navbar-brand'>{{pageTitle}}</a>
+    <ul class='nav nav-pills'>
+      <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+      <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
+    </ul>
+  </nav>
+  `
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from 'src/products/product-list.component';
+import { RouterModule }  from '@angular/router';
+import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/conver-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path : 'products', component: ProductListComponent },
+      { path : 'products/:id', component: ProductDetailComponent },
+      { path : 'welcome', component: WelcomeComponent },
+      { path : '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path : '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
