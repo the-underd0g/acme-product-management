9b5e11579ba072b5f55eac0adb1ffb300b68b02b
UnderD0g <the.underd0g@outlook.com>
2019-01-06 17:08:35 -0300

    adding routing, and guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,9 @@ import { WelcomeComponent } from './home/welcome.component';
     HttpClientModule,
     RouterModule.forRoot([
       { path : 'products', component: ProductListComponent },
-      { path : 'products/:id', component: ProductDetailComponent },
+      { path : 'products/:id', 
+          canActivate: [ ProductDetailGuard ],
+          component: ProductDetailComponent },
       { path : 'welcome', component: WelcomeComponent },
       { path : '', redirectTo: 'welcome', pathMatch: 'full'},
       { path : '**', redirectTo: 'welcome', pathMatch: 'full'}
