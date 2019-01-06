4dabd3dfba27e52f7be0cc08db08672613159336
UnderD0g <the.underd0g@outlook.com>
2019-01-06 10:29:02 -0300

    adding product interface and class

--- src/products/product-list.component.ts
+++ src/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
@@ -10,8 +11,8 @@ export class ProductListComponent {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    
-    products: any[] = [
+
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
